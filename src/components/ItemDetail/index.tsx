680169b972ce8c269b022511b7561a8ae67ae497
Daniel Perez <daniel.perez@Daniels-MacBook-Pro.local>
2021-06-15 00:58:47 -0500

    feat: itema detail was implemented

--- src/components/ItemDetail/index.tsx
+++ src/components/ItemDetail/index.tsx
@@ -14,37 +14,37 @@ const ItemDetail = ({ item }: ItemDetailProps) => {
 
   return (
     <S.Wrapper>
-      <div className="meli-app__item-detail__image">
+      <S.WrapperImage>
         <Image width={'100%'} height={'100%'} src={picture} alt="Item Image" />
-      </div>
-      <div className="meli-app__item-detail__pricebox">
-        <div className="meli-app__pricebox">
-          <div className="meli-app__pricebox__state">
-            <label>
+      </S.WrapperImage>
+      <S.WrapperPricebox>
+        <S.Pricebox>
+          <S.PriceboxState>
+            <span>
               {condition === 'new' ? 'Nuevo' : 'Usado'} - {sold_quantity}{' '}
               Vendidos
-            </label>
-          </div>
-          <div className="meli-app__pricebox__title">{title}</div>
-          <div className="meli-app__pricebox__price">
+            </span>
+          </S.PriceboxState>
+          <S.PriceboxTitle>{title}</S.PriceboxTitle>
+          <S.PriceboxPrice>
             <NumberFormat
               value={price ? price.amount : 0}
               displayType="text"
               thousandSeparator={true}
               prefix={'$ '}
             />
-          </div>
-          <div className="meli-app__pricebox__buy">
+          </S.PriceboxPrice>
+          <S.PriceboxBuy>
             <button type="button" disabled={true}>
               Comprar
             </button>
-          </div>
-        </div>
-      </div>
-      <div className="meli-app__item-detail__description">
+          </S.PriceboxBuy>
+        </S.Pricebox>
+      </S.WrapperPricebox>
+      <S.Description>
         <h3>Descripción del producto</h3>
         <p>{description}</p>
-      </div>
+      </S.Description>
     </S.Wrapper>
   )
 }
--- src/components/ItemDetail/styles.ts
+++ src/components/ItemDetail/styles.ts
@@ -1,3 +1,98 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background: #ffffff;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: auto auto;
+    max-width: ${theme.breakPoints.desktop};
+    width: 100%;
+  `}
+`
+
+export const WrapperImage = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    gap: 20px;
+    grid-column: 1/8;
+    grid-template-rows: repeat(6, 4rem);
+    padding: 32px;
+    > div {
+      grid-row: 1/7;
+      > div {
+        height: 100%;
+        width: 100%;
+      }
+    }
+
+    img {
+      object-fit: fill;
+    }
+  `}
+`
+export const WrapperPricebox = styled.div`
+  ${({ theme }) => css`
+    grid-column: 10/13;
+  `}
+`
+export const Pricebox = styled.div`
+  ${({ theme }) => css`
+    padding: 32px;
+  `}
+`
+export const PriceboxState = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.medium}
+    padding-bottom: 16px;
+  `}
+`
+export const PriceboxTitle = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.headline4}
+    padding-bottom: 32px;
+  `}
+`
+export const PriceboxPrice = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.headline4}
+    padding-bottom: 32px;
+  `}
+`
+export const PriceboxBuy = styled.div`
+  ${({ theme }) => css`
+    button {
+      background-color: ${theme.colors.primaryBlue};
+      color: #ffffff;
+      border-radius: 4px;
+      border: none;
+      outline: none;
+      width: 100%;
+      padding: 8px;
+      border-style: none;
+      cursor: pointer;
+      &:hover,
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+    }
+  `}
+`
+export const Description = styled.div`
+  ${({ theme }) => css`
+    grid-column: 1/13;
+    padding: 0 32px 32px 32px;
+    h3 {
+      ${theme.typography.headline4}
+      color: ${theme.colors.textDark};
+      font-weight: 500;
+      padding-bottom: 32px;
+    }
+    p {
+      ${theme.typography.medium}
+      color: ${theme.colors.text};
+    }
+  `}
+`
